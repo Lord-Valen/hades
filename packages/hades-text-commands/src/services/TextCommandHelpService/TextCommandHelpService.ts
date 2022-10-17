5cad5ded1fd6eec1e327d5ac27099a748c7ef773
Lord-Valen <lord_valen@protonmail.com>
2022-10-16 22:22:09 -0400

    :brain: `redesign` Use monorepo
    Brought the text-commands extension in as a workspace, updating discord.js to 14.x

---
+++ packages/hades-text-commands/src/services/TextCommandHelpService/TextCommandHelpService.ts
@@ -0,0 +1,41 @@
+import { EmbedBuilder } from "discord.js";
+import { inject } from "inversify";
+import { singleton } from "hades";
+import { TextCommandHelper } from "./TextCommandHelper";
+import { TextCommandHelperRegistry } from "./TextCommandHelperRegistry";
+
+
+@singleton(TextCommandHelpService)
+export class TextCommandHelpService {
+
+  @inject(TextCommandHelperRegistry)
+  helpers: TextCommandHelperRegistry
+
+  getHelpEmbed(command: string) {
+    const helper = this.helpers.helperFor(command)
+
+    if (helper) {
+      return helper.getHelpEmbed()
+    }
+  }
+
+  getCommandsEmbed() {
+    let embed = new EmbedBuilder()
+    const undocumented: TextCommandHelper[] = []
+
+    for (const helper of this.helpers.helpers) {
+      if (helper.args.size > 0 || helper.description) {
+        embed = embed.addFields([{ name: helper.getUsage(), value: helper.description }])
+      } else {
+        undocumented.push(helper)
+      }
+    }
+
+    embed = embed.addFields([{
+      name: "Other commands:",
+      value: undocumented.map(helper => helper.name).join(", ")
+    }])
+
+    return embed;
+  }
+}
